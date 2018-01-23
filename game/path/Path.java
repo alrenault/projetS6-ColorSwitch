19f7255a8dd6800db7347f49e6f59ae92e8ee1ce
Alexis <alexis.renault1@etu.univ-orleans.fr>
2018-01-23 13:28:35 +0100

    continuation du path et generator + creation Menu.java

--- game/path/Path.java
+++ game/path/Path.java
@@ -1,5 +1,6 @@
 package game.path;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import game.ennemy.Ennemy;
@@ -7,11 +8,51 @@ import game.ennemy.Ennemy;
 
 
 public class Path {
-	List<Obstacle> obstacles;
-	List<Ennemy> ennemies;
+	private List<Obstacle> obstacles;
+	private List<Ennemy> ennemies;
+	
+	public Path(){
+		obstacles = new ArrayList<Obstacle>();
+		ennemies = new ArrayList<Ennemy>();
+	}
 	
 	public Path(List<Obstacle> _obstacles, List<Ennemy> _ennemies){
 		obstacles = _obstacles;
 		ennemies = _ennemies;
 	}
+	
+	public void add(Obstacle o){
+		obstacles.add(o);
+	}
+	
+	public void add(Ennemy e){
+		ennemies.add(e);
+	}
+	
+	public void remove(Obstacle o){
+		obstacles.remove(o);
+	}
+	
+	public void remove(Ennemy e){
+		ennemies.remove(e);
+	}
+
+	public isCorrect(){
+		
+	}
+	
+	
+	
+	/*c'est pas cool !!!*/
+	//TODO
+	public List<Obstacle> getObstacles() {
+		return obstacles;
+	}
+
+	//TODO
+	public List<Ennemy> getEnnemies() {
+		return ennemies;
+	}
+	
+	
 }
--- model/Generator.java
+++ model/Generator.java
@@ -1,5 +1,19 @@
 package model;
 
+import game.Game;
+import game.ball.Ball;
+import game.law.Universe;
+import game.path.Path;
+
+
 public class Generator {
+	
+	private Game game;
+	
+	public Generator(Ball _balle, Path _path, Universe _universe){
+		game = new Game(_balle, _path, _universe);
+	}
+	
+	
 
 }
