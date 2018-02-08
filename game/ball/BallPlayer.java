8c8c2bc5c81fc286116339bf26e3c00faab85dd5
J <J>
2018-02-08 23:16:11 +0100

    Gravité et Saut à améliorer :'(

--- game/ball/BallPlayer.java
+++ game/ball/BallPlayer.java
@@ -2,7 +2,12 @@ package game.ball;
 
 import controller.Listeners;
 import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
@@ -61,33 +66,64 @@ public class BallPlayer extends Ball {
     public void setColor(Color color) {
         this.color = color;
     }
+    
+    int x = 70;
 
     @Override
     public void applyGravity() {
         System.out.println("Gravity Ball");
-        gravity = new TranslateTransition(Duration.seconds(4), shape);
+        gravity = new TranslateTransition(Duration.seconds(2), shape);
         gravity.setByY(scene.getHeight() + size);
         //tt1.setCycleCount(4);
-        gravity.setCycleCount((int) Double.POSITIVE_INFINITY);//mouvement a l'infini
-        //tt1.setAutoReverse(true);
+        //gravity.setCycleCount((int) Double.POSITIVE_INFINITY);//mouvement a l'infini
+        gravity.setAutoReverse(false);
         gravity.setInterpolator(Interpolator.LINEAR);
-        gravity.play();
+        //gravity.play();
 
     }
 
+    //static int xj = 100;
+    
     @Override
     public void jump() {
         // TODO Auto-generated method stub
         System.out.println("Jump Ball");
         gravity.pause();
-        TranslateTransition tt1 = new TranslateTransition(Duration.seconds(1), shape);
-        tt1.setByY(-50);
+        gravity.stop();
+        TranslateTransition tt1 = new TranslateTransition(Duration.millis(150), shape);
+        tt1.setByY(-x);
         tt1.setCycleCount(1);
         //tt1.setCycleCount((int)Double.POSITIVE_INFINITY);//mouvement a l'infini
-        tt1.setAutoReverse(true);
-        tt1.setInterpolator(Interpolator.LINEAR);
+        tt1.setAutoReverse(false);
+        tt1.setInterpolator(Interpolator.EASE_OUT);
+        tt1.setOnFinished(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				gravity.play();	
+			}	
+        });
+        tt1.stop();
         tt1.play();
-        gravity.play();
+        
+        
+        
+        /*
+        Timeline time = new Timeline();
+        KeyValue kv = new KeyValue(shape.translateXProperty(), 2);
+        KeyFrame ke = new KeyFrame(Duration.millis(1000),kv);
+        //time.getKeyFrames().add(ke);
+        
+        xj+=100;
+
+        time.getKeyFrames().addAll(
+                new KeyFrame(Duration.ZERO, // set start position at 0
+                   new KeyValue(shape.translateXProperty(), shape.getTranslateX()),
+                   new KeyValue(shape.translateYProperty(), shape.getLayoutY()-x)),
+
+                new KeyFrame(new Duration(400), // set end position at 40s
+                   new KeyValue(shape.translateXProperty(), 0),
+                   new KeyValue(shape.translateYProperty(), -x,Interpolator.EASE_OUT)));
+        time.play();
+        */
 
     }
 
