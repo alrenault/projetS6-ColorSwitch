2bd6599bf73fee99b44f5fa916be8faa82415e5c
V1nc3n7 <vincent.renard@gmx.com>
2018-02-09 22:50:22 +0100

    -Remove old colours

--- game/Colorable.java
+++ game/Colorable.java
@@ -2,31 +2,24 @@ package game;
 
 import javafx.scene.paint.Color;
 
-import java.util.Random;
-
 /**
  * This interface propose to use already defined colors when building shapes
  */
 public interface Colorable {
-	Color[] normal = {Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW};
-	Color[] hard = {Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW, Color.MAGENTA, Color.ORANGE};
-	Color[] randomNormal = {
-			new Color((new Random()).nextDouble(), (new Random()).nextDouble(), (new Random()).nextDouble(), 1),
-			new Color((new Random()).nextDouble(), (new Random()).nextDouble(), (new Random()).nextDouble(), 1),
-			new Color((new Random()).nextDouble(), (new Random()).nextDouble(), (new Random()).nextDouble(), 1),
-			new Color((new Random()).nextDouble(), (new Random()).nextDouble(), (new Random()).nextDouble(), 1)};
-	
-	Color BLACK = Color.rgb(39,39,39);
-	Color YELLOW = Color.rgb(255,255,0);
-	Color PURPLE = Color.rgb(144,13,255);
-	Color ROSE = Color.rgb(255,1,129);
-	Color BLUE = Color.rgb(50,219,240);
-	Color[] WHITE = {Color.rgb(255, 255, 255)};
-	
-	Color[] CUSTOM = {YELLOW,PURPLE,ROSE,BLUE};
+
+    Color BLACK = Color.rgb(39, 39, 39);
+    Color YELLOW = Color.rgb(255, 255, 0);
+    Color PURPLE = Color.rgb(144, 13, 255);
+    Color ROSE = Color.rgb(255, 1, 129);
+    Color BLUE = Color.rgb(50, 219, 240);
 
 
-	//public void color(Node n);
+    Color[] WHITE = {Color.rgb(255, 255, 255)};
+
+    Color[] CUSTOM = {YELLOW, PURPLE, ROSE, BLUE};
+
+
+    //public void color(Node n);
 
 
 }
