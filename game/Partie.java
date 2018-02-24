2a8e4aaddcdf4177b02d05d6b6f8897ebee6d89b
V1nc3n7 <vincent.renard@gmx.com>
2018-02-24 13:45:06 +0100

    + partie file

---
+++ game/Partie.java
@@ -0,0 +1,17 @@
+package game;
+
+
+import game.path.Path;
+import javafx.scene.paint.Color;
+import model.Model;
+
+
+public class Partie {
+    private Model model;
+    private Path path;
+
+    private Color backGroundColor;
+
+
+
+}
