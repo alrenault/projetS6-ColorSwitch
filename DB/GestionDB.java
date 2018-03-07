7ea0f8c432404a8e699581f1c5f74bfae940b785
V1nc3n7 <vincent.renard@gmx.com>
2018-03-07 16:20:35 +0100

    add fuction select like for a player

--- DB/GestionDB.java
+++ DB/GestionDB.java
@@ -3,6 +3,11 @@ package DB;
 import game.Score;
 
 import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @autor Vincent
@@ -22,12 +27,15 @@ public class GestionDB {
         this.connexion();
     }
 
-
+    /**
+     * Doit etre uniquement utilisée dans et par cette classe
+     * Effectue la connexion sql
+     */
     private void connexion(){
         try {
-            if (this.connexion==null||this.connexion.isClosed()) {
+            if (connexion==null||connexion.isClosed()) {
                     Class.forName(JDBC_DRIVER);
-                    this.connexion = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+                    connexion = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
             }
         }
         catch (SQLException e) {
@@ -39,12 +47,14 @@ public class GestionDB {
     }
 
     /**
-     *
-     * @param pseudo
-     * @return
+     *Teste l'existence d'un pseudo dans la base
+     * @param pseudo le pseudo à rechercher
+     * @return true si pseudo present
      */
     private boolean pseudoInDB(String pseudo){// si le pseudo est dans la db
-        System.err.println("pseudo in db : "+pseudo);
+        assert(pseudo.length()>2);
+        assert(pseudo.length()<25);
+
         try {
             connexion();
             PreparedStatement stmt = connexion.prepareStatement("SELECT ID_user from user WHERE pseudo_user = ?");
@@ -67,7 +77,7 @@ public class GestionDB {
      * @param pseudo
      * @param scoreToInsrt
      */
-    public void insert(String pseudo, Score scoreToInsrt){
+    public void record(String pseudo, Score scoreToInsrt){
 
 
 
@@ -98,5 +108,46 @@ public class GestionDB {
                 e.printStackTrace();
             }
         }
+
+        public List<Record> getScoresOfPlayer(String pseudoJoueur){
+
+           if (!(pseudoInDB(pseudoJoueur)))return null;
+
+            List<Record> ret=new LinkedList<>();
+            connexion();
+            PreparedStatement stmt = null;
+            try {
+                stmt = connexion.prepareStatement("SELECT pseudo_user,date_heur_partie,nb_portes_traversees_partie," +
+                        " nb_etoiles_ramassee_partie,score_partie from partie NATURAL JOIN user WHERE pseudo_user = ?",ResultSet.TYPE_FORWARD_ONLY,ResultSet.CONCUR_READ_ONLY);
+
+                stmt.setString(1,pseudoJoueur);
+
+                ResultSet reponse = stmt.executeQuery();
+                while (reponse.next()) {
+
+                    SimpleDateFormat patern = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    String datett = patern.format(reponse.getTimestamp("date_heur_partie"));
+                   /*
+                    System.err.println(reponse.getString("pseudo_user")+" "+
+                            new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(reponse.getTimestamp("date_heur_partie"))
+                            +" "+reponse.getInt("nb_portes_traversees_partie")
+                    +" "+reponse.getInt("nb_etoiles_ramassee_partie")+" "+reponse.getInt("score_partie"));*/
+
+
+                    ret.add(new Record(reponse.getString("pseudo_user"),new Score(reponse.getInt("nb_portes_traversees_partie"),reponse.getInt("nb_etoiles_ramassee_partie"),reponse.getInt("score_partie")),datett));
+
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return ret;
+        }
+
+
+
+
+
+
+
     }
 
