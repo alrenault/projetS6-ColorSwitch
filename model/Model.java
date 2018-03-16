34c0805ed67a572abc8f06c29def156668df2abe
J <J>
2018-03-16 18:57:53 +0100

    Restructuration sans Game

--- model/Model.java
+++ model/Model.java
@@ -1,47 +1,78 @@
 package model;
 
-import javafx.scene.Scene;
+import game.Game;
+import game.ball.Ball;
+import game.law.Universe;
+import game.path.Path;
+import controller.Controller;
+import game.Difficulty;
 
 /**
  * This class represents the model in the MVC pattern
  * It contains ALL the data needed for the view
  */
-
-public class Model {
-    /**
-     * points gagnés par ramassage d'etoiles
-     */
-
-    public int points_star;
-    /**
-     * Taille de la fenetre en X
-     */
-    public double screen_sizeX;
-    /**
-     * Taille de la fenetre en Y
-     */
-    public double screen_sizeY;
-    
-    public Model() {
-    	
-    }
-    
-    
-
-
-    public Model(double screen_sizeX, double screen_sizeY, int points_star) {
-        this.screen_sizeX = screen_sizeX;
-        this.screen_sizeY = screen_sizeY;
-        this.points_star = points_star;
-    }
-
-    public int getPoints_star() {
-        return points_star;
-    }
-
-
-	public void createGame(Scene scene) {
-		// TODO Auto-generated method stub
+public class Model implements InterfaceColorSwitch {
+	
+	private Game game;
+	
+	public Model() {
 		
 	}
+	
+	@Override
+	public void startColorSwitch() {
+		// TODO Auto-generated method stub
+		
+		//Charger le Menu
+		
+	}
+	
+	
+
+	@Override
+	public void startGame() {
+		// TODO Auto-generated method stub
+		
+		//Lancer une partie
+		this.game = new Game();
+		
+	}
+
+	@Override
+	public void gameOver() {
+		// TODO Auto-generated method stub
+		
+		//Perdre la partie
+		
+	}
+
+	@Override
+	public void jump() {
+		// TODO Auto-generated method stub
+		
+		//Faire sauter la balle
+		game.jump();
+		
+	}
+
+	@Override
+	public void registerScore() {
+		// TODO Auto-generated method stub
+		
+		//Enregistrer le score
+		
+	}
+
+	@Override
+	public void startGame(Difficulty difficulty, boolean infinite, Path path, Universe universe, Ball ball) {
+		// TODO Auto-generated method stub
+		
+		this.game = new Game();
+		
+	}
+
+	public Game getGame() {
+		return game;
+	}
+	
 }
--- view/View.java
+++ view/View.java
@@ -1,16 +1,49 @@
 package view;
 
+import game.Game;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
-public class View {
-
-	public View() {
-		
+public class View implements InterfaceViewJavaFX {
+	
+	Stage stage;
+	int tailleX = 600;
+	int tailleY = 1000;
+	//Preferences
+	
+	public View(Stage primaryStage) {
+		stage = primaryStage;
+		initStage(stage);
+		viewMenu();
 	}
 
-	public void viewGame(Scene scene) {
+	public View() {
+		this(new Stage());
+	}
+	
+	
+
+	public void initStage(Stage stage) {
+		//Set Stage
+        stage.setTitle("ColorSuitch");
+        stage.getIcons().add(new Image("file:../view/color_icon.png"));
+        stage.setFullScreen(1==0);
+	}
+	
+	private void viewMenu() {
 		// TODO Auto-generated method stub
 		
 	}
+
+	public void viewGame(Game thisGame) {
+		
+		Group root = new Group();
+		Scene scene = new Scene(root);
+		
+		ViewGameManagement.view(thisGame,scene);
+		
+	}
+	
 }
