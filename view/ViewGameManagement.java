f5e52bb85299d870785bc5b41191e96a486675c7
J <J>
2018-03-16 18:58:28 +0100

    Interface Vue

---
+++ view/ViewGameManagement.java
@@ -0,0 +1,11 @@
+package view;
+
+import game.Game;
+import javafx.scene.Scene;
+
+public class ViewGameManagement {
+
+	public static void view(Game thisGame, Scene thisScene) {
+		//Remplir scene avec game
+	}
+}
