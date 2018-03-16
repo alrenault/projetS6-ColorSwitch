dfff4c4bc257e2328573c3114cef240b853089d1
J <J>
2018-03-16 21:32:57 +0100

    Avancement du Controleur, rename des interfaces

---
+++ view/InterfaceViewColorSwitch.java
@@ -0,0 +1,13 @@
+package view;
+
+import game.Game;
+
+public interface InterfaceViewColorSwitch {
+
+	public void basicView();
+	
+	public void viewMenu() ;
+
+	public void viewGame(Game thisGame);
+	
+}
