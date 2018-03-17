c83fd7cea088fe33348cb6d9535eac90cf853921
J <J>
2018-03-17 04:51:32 +0100

    Séparer le modèle et les composantes graphiques : Initialisation

--- view/ViewGameManagement.java
+++ view/ViewGameManagement.java
@@ -4,22 +4,29 @@ import game.Colorable;
 import game.Difficulty;
 import game.Game;
 import game.path.Path;
+import game.path.obstacle.Obstacle;
+import game.path.shapes.Shapes;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 
 public class ViewGameManagement {
 	
 	static Group root;
+	static Game game;
 
 	public static void view(Game thisGame, Scene thisScene) {
 		
         root = (Group) thisScene.getRoot();
+        game = thisGame;
 		
 		
-		Path p = thisGame.getPath();
+		Path path = game.getPath();
+		
+		Group jObstacles = buildObstacles(path);
 
         
-        add(p.getPath());
+        add(path.getPath());
+        add(jObstacles);
         
         
         
@@ -29,5 +36,19 @@ public class ViewGameManagement {
 	
 	private static void add(Group group) {
 		root.getChildren().add(group);
+	}
+	
+	public static Group buildObstacles(Path path) {
+		Group jObstacles = new Group();
+		
+		for(Obstacle obstacle : path.getObstacles()) {
+			//obstacle -> group
+			//ajouter à jObstacles
+			
+			
+		}
+		
+		
+		return jObstacles;
 	}
 }
