7b69c5ab3210b59f91c7b2437adf3a8edb5dc0f3
V1nc3n7 <vincent.renard@gmx.com>
2018-03-18 21:40:19 +0100

    Ajout fonction testConnexionDB + MainGestionDB plus clair

--- main/MainTestDB.java
+++ main/MainTestDB.java
@@ -14,35 +14,56 @@ import java.util.LinkedList;
  */
 public class MainTestDB {
     public static void main(String[] args) {
-        GestionDB gestion =new GestionDB();
+        GestionDB gestion = new GestionDB();
+        String TEST = "";
+
+
+
+        TEST="Connextion";
+        System.out.println(TEST);
+        if (gestion.testConnexionDB()){
+
+        System.out.println("\u001B[32m" +"Felicitation La connexion est possible "+"\u001B[0m");
+        }else {
+            System.out.println("\u001B[31m" +"Pas de chance  Envoie moi un message..."+"\u001B[0m");
+
+        }
+
+
+        TEST="Test du top joueurs";
+        System.out.println(TEST);
         LinkedList<SumScore> l =gestion.topJoueurs();
-        System.out.println("Test du top joueurs");
         for (SumScore s : l) {
             //System.err.println(s.toString());
             System.out.println(s.toString());
         }
-        System.out.println("FinTest du top joueurs");
+
+        System.out.println(TEST+((l.size()==17)?"\u001B[32m" +" OK"+"\u001B[0m":"\u001B[31m" +" PAS_OK"+"\u001B[0m"));
+        System.out.println("Fin"+TEST);
 
 
-        System.out.println("Test du getScoreOfPlayer");
+        TEST="Test du getScoreOfPlayer";
+        System.out.println(TEST);
         LinkedList<Record> la =gestion.getScoresOfPlayer("Alexis");
         for (Record r : la) {
             //System.err.println(s.toString());
             System.out.println(r.toString());
         }
-        System.out.println("FinTest du getScoreOfPlayer");
 
+        System.out.println(TEST+((!(la.isEmpty())?"\u001B[32m" +" OK"+"\u001B[0m":"\u001B[31m" +" PAS_OK"+"\u001B[0m")));
+        System.out.println("Fin"+TEST);
 
-        System.out.println("Test du getNBestRecords");
+        TEST="getNBestRecords";
+        System.out.println(TEST);
         LinkedList<Record> lr = gestion.getNBestRecords(33);
         int i = 1;
         for (Record re : lr) {
 
-            //System.err.println(s.toString());
             System.out.println(i++ +re.toString());
         }
-
-        System.out.println("FinTest du getNBestRecords");
+        System.out.println(TEST+((!(lr.isEmpty())?"\u001B[32m" +" OK"+"\u001B[0m":"\u001B[31m" +" PAS_OK"+"\u001B[0m")));
+        System.out.println("Fin"+TEST);
+        System.out.println("Fin des tests");
 
     }
 }
