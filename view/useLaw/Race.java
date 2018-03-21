8f0d9cdb9bbfeea6f46e5f5d9ffa18410d819363
J-Lyy <J-Lyy@users.noreply.github.com>
2018-03-21 13:24:41 +0100

    Mode Race
    Raciste

---
+++ view/useLaw/Race.java
@@ -0,0 +1,30 @@
+package view.useLaw;
+
+
+import game.ball.BallPlayer;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.Scene;
+
+public class Race implements UseLaw {
+
+	BallPlayer ball;
+	Scene scene;
+	
+	public Race(BallPlayer ball, Scene scene) {
+		this.ball = ball;
+	}
+	
+	@Override
+	public void apply() {
+		// TODO Auto-generated method stub
+
+		ball.getShape().setTranslateY(ball.getShape().getTranslateY()-5);
+		
+		//scene.setOnMouseDragged(event -> System.out.println("Hello"));
+		
+		
+		//ball.getShape().setTranslateX(ball.getShape().getTranslateY()-5);
+
+	}
+
+}
