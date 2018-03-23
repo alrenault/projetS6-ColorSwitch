f40430f309a1fa529ad455c21229707bd34fcdfe
Oui <Lyy.Q1000@gmail.com>
2018-03-23 12:49:15 +0100

    Début d'IA

---
+++ view/useLaw/IAJump.java
@@ -0,0 +1,21 @@
+package view.useLaw;
+
+import game.ball.BallPlayer;
+import javafx.event.Event;
+import view.ViewPath;
+import javafx.scene.input.*;
+
+public class IAJump implements UseLaw {
+
+	ViewPath viewpath;
+	BallPlayer ball;
+	
+	@Override
+	public void apply() {
+		//KeyEvent space = new KeyEvent();
+		//EventTarget eventTarget;
+		//Event.fireEvent(eventTarget, new KeyEvent());
+
+	}
+
+}
